90a8cb47c2c01316214f146a62c7710ca47262a4
moibenavides2-create <moibenavides2@gmail.com>
2026-08-10 16:46:53 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,12 +12,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDv9Q2x4UTLxNu334r67cVo_MLh8gDMhiE",
+  authDomain: "logos-theos.firebaseapp.com",
+  projectId: "logos-theos",
+  storageBucket: "logos-theos.firebasestorage.app",
+  messagingSenderId: "51795363",
+  appId: "1:51795363:web:74a3352b8e5832fa6fb616"
 };
 
 export const app = initializeApp(firebaseConfig);
